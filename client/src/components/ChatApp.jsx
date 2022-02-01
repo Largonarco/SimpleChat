578fc400f5bb71556598cfbd694203303f507161
Largonarco <snarayankars62@gmail.com>
2022-02-01 20:34:51 +0530

    production

--- client/src/components/ChatApp.jsx
+++ client/src/components/ChatApp.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", { autoConnect: false });
+const socket = io({ autoConnect: false });
 
 const ChatApp = ({ userData }) => {
   const [messages, setMessages] = useState([]);
